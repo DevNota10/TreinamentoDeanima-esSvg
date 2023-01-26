439171206515c073bc19f819de67abdc90b012b0
DevNota10 <dev.nota10.k@gmail.com>
2023-01-26 19:36:40 -0300

    TreinoAnimaçoesSvg

---
+++ script.js
@@ -0,0 +1,7 @@
+const tagSvg = document.querySelector('svg');
+
+const pathDoSvg = document.querySelector('svg path');
+const tamanhoTotalDopath = pathDoSvg.getTotalLength();
+
+
+console.log(tamanhoTotalDopath)
